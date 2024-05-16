d74df953ac178cc024dde6cd84e4ce9e9444e440
shanmugapriyakishore <shanmugap1999@gmail.com>
2024-05-16 11:40:19 +0530

    modified index and added script in task1

---
+++ task 1/script.js
@@ -0,0 +1,30 @@
+function clearDisplay() {
+    document.getElementsByName('display')[0].value = '';
+}
+
+function deleteLast() {
+    var currentValue = document.getElementsByName('display')[0].value;
+    document.getElementsByName('display')[0].value = currentValue.slice(0, -1);
+}
+
+
+function appendDot() {
+    document.getElementsByName('display')[0].value += '.';
+}
+
+
+function appendOperator(operator) {
+    document.getElementsByName('display')[0].value += operator;
+}
+
+
+function appendDigit(digit) {
+    document.getElementsByName('display')[0].value += digit;
+}
+
+
+function evaluateExpression() {
+    var expression = document.getElementsByName('display')[0].value;
+    var result = eval(expression);
+    document.getElementsByName('display')[0].value = result;
+}
